5261ca6faadf12341aeb872a66332f7336369b00
ShayanTafrishi <shayantafrishi@gmail.com>
2021-03-15 11:24:26 +0100

    door.c og door.h skrevet ferdig

---
+++ door.h
@@ -0,0 +1,34 @@
+#indef DOOR_H_
+#define DOOR_H_
+#include "timer.h"
+/**
+*@file
+*@brief A executive controlling operations connected to the doors
+*/
+
+int door_keep_open();
+
+
+
+/**
+* @brief Open the door and set the @p
+* is_door_open int to 1.
+*/
+void door_open();
+
+/**
+* @brief Close the door and set the @p
+* is_door_open int to 0.
+*
+*/
+void door_close();
+
+/**
+* @brief Fetch door status.
+*
+* @return 0 when door is closed
+* * @return 1 when door i open.
+*/
+int door_get_status();
+
+#endif
