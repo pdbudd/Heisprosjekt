442fedf70b5690d64d36ca7b859434c8386afce4
Philip Budd <philip.budd@gmail.com>
2021-03-16 18:50:50 +0100

    testing screenshots

--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -14,7 +14,7 @@ int main(){
         exit(1);
     }
     int currentfloor = initialise();
-    current_direction = HARDWARE_MOVEMENT_STOP;
+    HardwareMovement current_direction = HARDWARE_MOVEMENT_STOP;
 
     while(1){
         if(hardware_read_stop_signal()){
@@ -30,36 +30,6 @@ int main(){
               hardware_command_floor_indicator_on(f);
               floor_reached(f);
           }
-        }
-
-
-
-
-        /* Lights are set and cleared like this: */
-        for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
-            /* Internal orders */
-            if(hardware_read_order(f, HARDWARE_ORDER_INSIDE)){
-                hardware_command_order_light(f, HARDWARE_ORDER_INSIDE, 1);
-            }
-
-            /* Orders going up */
-            if(hardware_read_order(f, HARDWARE_ORDER_UP)){
-                hardware_command_order_light(f, HARDWARE_ORDER_UP, 1);
-            }
-
-            /* Orders going down */
-            if(hardware_read_order(f, HARDWARE_ORDER_DOWN)){
-                hardware_command_order_light(f, HARDWARE_ORDER_DOWN, 1);
-            }
-        }
-
-        /* Code to clear all lights given the obstruction signal */
-        if(hardware_read_obstruction_signal()){
-            hardware_command_stop_light(1);
-            clear_all_order_lights();
-        }
-        else{
-            hardware_command_stop_light(0);
         }
     }
 
