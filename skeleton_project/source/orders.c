bcf0f9de972a7f451f6af283aa291bab6c4633c6
Philip Budd <philip.budd@gmail.com>
2021-03-18 11:52:47 +0100

    js

--- skeleton_project/source/orders.c
+++ skeleton_project/source/orders.c
@@ -85,6 +85,7 @@ int floor_stop_query()
 void new_direction()
 {
   printf("choosing direction\n");
+  printf("%s", previous_direction);
   switch (previous_direction)
   {
     case HARDWARE_MOVEMENT_UP:
